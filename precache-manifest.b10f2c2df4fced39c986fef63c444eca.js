91a4ab771a2c8dbcb8afa558aa0e5a557262c9ab
Grant Brewster <grant@Grants-MacBook-Pro.local>
2019-09-20 01:44:02 -0400

    Updates

---
+++ precache-manifest.b10f2c2df4fced39c986fef63c444eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f5d7c2527c02287f0d2e0d3ad990ac",
+    "url": "/PennGreekRelaysSite/index.html"
+  },
+  {
+    "revision": "279e44642dce9360c63b",
+    "url": "/PennGreekRelaysSite/static/css/main.82624d44.chunk.css"
+  },
+  {
+    "revision": "9beddafbb4be0b4cd285",
+    "url": "/PennGreekRelaysSite/static/js/2.f09bbac4.chunk.js"
+  },
+  {
+    "revision": "279e44642dce9360c63b",
+    "url": "/PennGreekRelaysSite/static/js/main.41d83c48.chunk.js"
+  },
+  {
+    "revision": "acf7f04a9336c5130de7",
+    "url": "/PennGreekRelaysSite/static/js/runtime-main.b4d8f736.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/PennGreekRelaysSite/static/media/logo.25bf045c.svg"
+  }
+]);
